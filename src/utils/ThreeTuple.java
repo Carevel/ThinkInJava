fa58bace9d907ae0e6c19ac7ef8c9a47440bf64d
Carevel <872287340@qq.com>
2016-06-29 23:15:27 +0800

    merge

---
+++ src/utils/ThreeTuple.java
@@ -0,0 +1,13 @@
+package utils;
+
+/**
+ * Created by Changjiang on 2016/6/29.
+ */
+public class ThreeTuple<A,B,C> extends TwoTuple<A,B> {
+    public final C c;
+
+    public ThreeTuple(A a, B b, C c) {
+        super(a, b);
+        this.c = c;
+    }
+}
